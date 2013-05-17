027c355b859409c66ef780829273a926ad5a5960
David <DavidGranados@fullentregas.com.ar>
2013-05-17 16:50:07 -0300

    Ficheros

---
+++ Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Ficheros</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td>Ruta: </td>
+                <td><asp:TextBox ID="tb_Ruta" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Texto: </td>
+                <td><asp:TextBox ID="tb_Texto" runat="server"></asp:TextBox></td>
+            </tr>
+        </table>
+        <br />
+        <div><asp:Button ID="btn_CrearTXT" runat="server" Text="Crear TXT" /></div>
+        <div><asp:Button ID="btn_leerTXT" runat="server" Text="Leer Txt" /></div>
+        <br />
+        <div><asp:Label ID="lbl_Mostrar" runat="server"></asp:Label></div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.vb
@@ -0,0 +1,37 @@
+﻿Imports System.IO
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+
+    Protected Sub btn_CrearTXT_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btn_CrearTXT.Click
+        Dim fichero As String = tb_Ruta.Text
+        Dim Texto As String = tb_Texto.Text
+        lbl_Mostrar.Text = ""
+
+        If fichero <> "" Then
+            Dim sw As New System.IO.StreamWriter(fichero)
+            sw.WriteLine(Texto)
+            sw.Close()
+        Else
+            lbl_Mostrar.Text = "Debe ingresar la ruta del fichero"
+        End If
+    End Sub
+
+    Protected Sub btn_leerTXT_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btn_leerTXT.Click
+        Dim fichero As String = tb_Ruta.Text
+        Dim Texto As String = tb_Texto.Text
+
+        If fichero <> "" Then
+            Dim sr As New System.IO.StreamReader(fichero)
+            Texto = sr.ReadToEnd()
+            sr.Close()
+            lbl_Mostrar.Text = Texto
+        Else
+            lbl_Mostrar.Text = "Debe ingresar la ruta del fichero"
+        End If
+    End Sub
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        tb_Ruta.Text = System.AppDomain.CurrentDomain.BaseDirectory
+    End Sub
+End Class
